c817368282d8d636b16fcd5b8f487e41e70261dc
C <earthhominid@gmail.com>
2023-04-13 15:15:04 -0300

    heroku

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,5 +1,6 @@
-// Path: ./config/env/production/server.js`
-export default ({ env }) => ({
+// Path: ./config/env/production/server.js
+
+module.exports = ({ env }) => ({
   proxy: true,
   url: env("MY_HEROKU_URL"), // Sets the public URL of the application.
   app: {
